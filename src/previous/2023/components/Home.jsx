48abaf9402290e8a02ca05fbab7d0ba7c001bcab
Aman Sharma <109227261+aman-pen@users.noreply.github.com>
2024-12-16 23:17:39 +0530

    Remove Register Now Button

--- src/previous/2023/components/Home.jsx
+++ src/previous/2023/components/Home.jsx
@@ -178,7 +178,7 @@ const Home = ({ theme }) => {
           </Typography>
 
         </Box>
-        <Box className="d-flex justify-content-center">
+        <Box className="d-none justify-content-center">
           <Box
             sx={{
               paddingBottom: {
