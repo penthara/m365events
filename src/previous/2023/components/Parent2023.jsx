a57b9deafd799e168b1a1dd9b70c83bed4aa1a51
Jenkins NS <jenkinsns@gmail.com>
2025-11-12 00:25:20 +0530

    Update Parent2023.jsx

--- src/previous/2023/components/Parent2023.jsx
+++ src/previous/2023/components/Parent2023.jsx
@@ -78,14 +78,14 @@ const Parent2023 = ({ theme }) => {
             </ErrorBoundary>
       <Box id="agenda" />
       {/* <ErrorBoundary> */}
-      {speakerData.length > 0 && sessionData.length > 0 && (
+      { /*speakerData.length > 0 && sessionData.length > 0 && (
         <Agenda
         theme={theme}
         speaker={speakerData}
         session={sessionData}
         defaultTrackData={defaultTrackData}
         />
-        )}
+        )*/}
       {/* </ErrorBoundary> */}
       <Box id="prizes" />
       <Prizes theme={theme} />
