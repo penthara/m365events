8f0f1ad5290be2a144ddaa9fbcebef485af327d7
Jenkins NS <jenkinsns@gmail.com>
2025-11-12 00:11:08 +0530

    Update EventData.jsx

--- src/previous/2023/services/EventData.jsx
+++ src/previous/2023/services/EventData.jsx
@@ -1,4 +1,4 @@
-const apiId = "mz1j3hwl";
+const apiId = "wb4qm9lf";
 
 const SpeakerApi = `https://sessionize.com/api/v2/${apiId}/view/Speakers`;
 const SessionApi = `https://sessionize.com/api/v2/${apiId}/view/Sessions`;
