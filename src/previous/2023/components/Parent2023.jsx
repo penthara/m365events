a4b34736e6cc0f7bcba5a02f1fdd26ff38d78a76
Jenkins NS <jenkinsns@gmail.com>
2025-11-13 01:20:17 +0530

    Update Parent2023.jsx

--- src/previous/2023/components/Parent2023.jsx
+++ src/previous/2023/components/Parent2023.jsx
@@ -56,9 +56,9 @@ const Parent2023 = ({ theme }) => {
       { <CountdownTimer /> }
       <Box id="about" />
       <About theme={theme} />
-      {/* <Eventinfostripe theme={theme} /> */}
-      {/* <Box id="Playlist" /> */}
-      {/* <VideoPlayer theme={theme} /> */}
+      { <Eventinfostripe theme={theme} /> }
+      { <Box id="Playlist" /> }
+      { <VideoPlayer theme={theme} /> }
       <ErrorBoundary>
         <Box id="speakers" />
         {KeynoteSpeakerData != null && KeynoteSpeakerData != undefined ? (
@@ -77,7 +77,7 @@ const Parent2023 = ({ theme }) => {
             />
             </ErrorBoundary>
       <Box id="agenda" />
-      {/* <ErrorBoundary> */}
+       <ErrorBoundary> 
       { speakerData.length > 0 && sessionData.length > 0 && (
         <Agenda
         theme={theme}
@@ -86,7 +86,7 @@ const Parent2023 = ({ theme }) => {
         defaultTrackData={defaultTrackData}
         />
         )}
-       {/* </ErrorBoundary> */}
+        </ErrorBoundary> 
       <Box id="prizes" />
       <Prizes theme={theme} />
       {/* <Box id="donate" />
